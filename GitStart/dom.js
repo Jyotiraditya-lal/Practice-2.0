5353ec68f1be13ad2f4d4b0720856eb9ab8ad6a7
Jyotiraditya-lal <vaibhavlal0211@gmail.com>
2023-05-04 01:14:33 +0530

    added dom.js

---
+++ GitStart/dom.js
@@ -0,0 +1,17 @@
+/*console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms);*/
+let headertitle=document.getElementById("header-title");
+console.log(headertitle);
+headertitle.textContent = "Hello";
+console.log(headertitle)
+let header=document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+let h2=document.getElementsByClassName("title");
+h2[0].style.color="green";
+h2[0].style.fontWeight="bold";
