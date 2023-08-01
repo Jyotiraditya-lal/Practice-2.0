c522ffdc8928f19a0053593b9e94ea48602ffa83
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-08-01 16:13:45 +0530

    Update redux-demo.js

--- redux-demo.js
+++ redux-demo.js
@@ -1,26 +1,12 @@
-const redux= require('redux')
-
 const reducer=(state={counter: 0} ,action)=>{
 
-    if(action.type==='increment'){
-        state.counter++
-    }else if(action.type==='decrement'){
-        state.counter--
+    if(action.type==='incrementby2'){
+        state.counter=state.counter+ 2
+    }else if(action.type==='decrementby2'){
+        state.counter=state.counter-2
     }
 
     return state
 }
 
 const store=redux.createStore(reducer)
-console.log(store.getState())
-
-const counterSub=()=>{
-
-    const latestState=store.getState()
-    console.log(latestState)
-}
-
-store.subscribe(counterSub)
-
-store.dispatch({type: 'increment'})
-store.dispatch({type: 'decrement'})
