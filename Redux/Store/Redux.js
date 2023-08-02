07ac3d3f70e35213b168ae6b2fc0a33df2aecf99
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-08-02 12:19:08 +0530

    Update and rename Redux/redux-demo.js to Redux/Store/Redux.js

--- Redux/Store/Redux.js
+++ Redux/Store/Redux.js
@@ -2,7 +2,7 @@ import { createSlice,configureStore } from '@reduxjs/toolkit'
 
 
 const UIstate={showCart: false}
-const CartState={items: [], totalPrice: 0, quantity: 0}
+const CartState={items: [], TotalQuantity: 0}
 
 const UIslice=createSlice({
     name: 'ui',
@@ -23,16 +23,31 @@ const CartSlice=createSlice({
         AddToCart(state,action){
             const newItem=action.payload;
             const exsistingItem=state.items.find(item=>item.id===newItem.id)
+            state.TotalQuantity++;
             if(!exsistingItem){
                 state.items.push({
                     id: newItem.id,
                     title: newItem.title,
                     quantity: 1,
-                    price: newItem.price
+                    price: newItem.price,
+                    totalPrice: newItem.price
                 })
             }else{
-                exsistingItem.quantity=exsistingItem.quantity+1;
-                exsistingItem.price=exsistingItem.price+newItem.price
+                exsistingItem.quantity++;
+                exsistingItem.totalPrice=exsistingItem.totalPrice + newItem.price
+            }
+        },
+
+        RemoveFromCart(state,action){
+
+            const id=action.payload
+            const exsistingItem=state.items.find(item=>id===item.id)
+            state.TotalQuantity--;
+            if(exsistingItem.quantity===1){
+                state.items=state.items.filter(item=>item.id!==id)
+            }else{
+                exsistingItem.quantity--;
+                exsistingItem.totalPrice=exsistingItem.totalPrice-exsistingItem.price
             }
         }
     }
