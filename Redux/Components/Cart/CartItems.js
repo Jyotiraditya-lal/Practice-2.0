366d929b6d7e12cf704b8eb6b3d76b0f6c3db0de
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-08-02 12:23:51 +0530

    Create CartItems.js

---
+++ Redux/Components/Cart/CartItems.js
@@ -0,0 +1,28 @@
+import classes from './CartItem.module.css';
+
+const CartItem = (props) => {
+  
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{props.title}</h3>
+        <div className={classes.price}>
+          ${props.total.toFixed(2)}{' '}
+          <span className={classes.itemprice}>(${props.price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{props.quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={props.onRemove}>-</button>
+          <button onClick={props.onAdd}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
