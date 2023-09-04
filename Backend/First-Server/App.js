489f60613649e4a769338492b9de227241af1bad
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-04 22:00:47 +0530

    Update App.js

--- Backend/First-Server/App.js
+++ Backend/First-Server/App.js
@@ -1,7 +1,22 @@
 const http=require('http')
 
 const rqListener=(req,res)=>{
-    console.log('vaibhav')
+    if(req.url==='/home'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to home page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }else if(req.url==='/about'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About us page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }else if(req.url==='/node'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my node project</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
 }
 
 const server=http.createServer(rqListener)
