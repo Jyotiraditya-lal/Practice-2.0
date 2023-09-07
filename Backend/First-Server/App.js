b2303e4704e2c974e33e3500ab5cdccd58274e07
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-07 20:13:11 +0530

    Update App.js

--- Backend/First-Server/App.js
+++ Backend/First-Server/App.js
@@ -1,57 +1,16 @@
 const http = require('http');
-const fs = require('fs');
+const express=require('express')
 
-const rqListener = (req, res) => {
-    if (req.url === '/') {
-        res.write('<!DOCTYPE html>');
-        res.write('<html>');
-        res.write('<head><title>Welcome</title></head>');
-        res.write('<body><form action="/message" method="POST" id="message-form"><input type="text" name="message" /><button type="submit">Send</button></form>');
-        
-        // Display the submitted message if available
-        if (req.method === 'POST') {
-            const body = [];
-            req.on('data', (chunk) => {
-                body.push(chunk);
-            });
+const app=express()
 
-            req.on('end', () => {
-                const parsedBody = Buffer.concat(body).toString();
-                const message = parsedBody.split('=')[1];
-                res.write(`<p>Message: ${message}</p>`);
-            });
-        }
-        
-        res.write('</body></html>');
-        return res.end();
-    }
-
-    if (req.url === '/message' && req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('Message.txt', message, (err) => {
-                if (err) {
-                    console.error('Error writing to file:', err);
-                } else {
-                    console.log('Message saved to Message.txt:', message);
-                }
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-};
-
-const server = http.createServer(rqListener);
-
-server.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.use((req,res,next)=>{
+    console.log('first middleware')
+    next()
 });
+
+app.use((req,res,next)=>{
+    console.log('second middleware')
+    res.send('<h1>Welcome to Expressjs</h1>')
+})
+
+app.listen(3000)
